4d60f9d5eab94232aa1da5a7e831961a2c8b1c92
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-12-03 16:07:22 -0800

    Use URL of MySQL through env var

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,7 +2,6 @@
 
 module.exports = function(server) {
   console.log('Started haoshiyou-server in NODE_ENV=' + process.env.NODE_ENV);
-
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
