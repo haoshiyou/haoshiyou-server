2aa4be53136e0f3e225dd2dd9b02297c39a36f0e
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-12-03 16:13:10 -0800

    Add mysql url check

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = function(server) {
-  console.log('Started haoshiyou-server in NODE_ENV=' + process.env.NODE_ENV);
+  if (!process.env.HSY_SERVER_MYSQL_URL) {
+    console.error('$HSY_SERVER_MYSQL_URL is not set, '
+      + 'set it by "export HSY_SERVER_MYSQL_URL=mysql://<user>:<pw>@<hostname>:<port>?reconnect=true"');
+    process.exit();
+  }
+  console.log('$HSY_SERVER_MYSQL_URL is set, now starting haoshiyou-server in NODE_ENV=' + process.env.NODE_ENV);
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
