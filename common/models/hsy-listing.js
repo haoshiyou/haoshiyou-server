235db03cbf38e0149ef6733e305a11e58e872956
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-12-15 20:39:18 -0800

    Add hook to sync location with location_lng and location_lat

--- common/models/hsy-listing.js
+++ common/models/hsy-listing.js
@@ -2,4 +2,15 @@
 
 module.exports = function(Hsylisting) {
 
+  Hsylisting.observe('before save', function (ctx, next) {
+    if (ctx.data == null || ctx.data == undefined) return next();
+    else if (ctx.data.location == null) { // set
+      ctx.data.location_lat = null;
+      ctx.data.location_lng = null;
+    } else if (ctx.data.location != undefined) { // uet
+      ctx.data.location_lat = ctx.data.location.lat;
+      ctx.data.location_lng = ctx.data.location.lng;
+    }
+    return next();
+  });
 };
