45a311cbabdcf6478109a002742fbcbe185c5f75
Zainan Victor Zhou <zzn@google.com>
2018-11-22 22:59:43 -0800

    Refactor to use async

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,32 +1,37 @@
 'use strict';
 
 console.assert(process.env.MONGODB_URL && process.env.MONGODB_DB, `Both MONGODB_URL and MONGODB_DB must be set`);
+
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  var MongoClient = require('mongodb').MongoClient;
+  // TODO: this is very hacky but let's just refactor later
+  new Promise(async (resolve, reject) => {
+    let client = await MongoClient.connect(process.env.MONGODB_URL);
+    var db = client.db(process.env.MONGODB_DB);
 
-  router.get('/ping', function(req, res) {
-    res.send('pong');
-  });
-  router.get('/GetStickyListings', function(req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-    // TODO: this is very hacky but let's just refactor later
-    MongoClient.connect(process.env.MONGODB_URL , function (err, client) {
-      if (err) throw err;
+    // Install a `/` route that returns server status
+    var router = server.loopback.Router();
+    router.get('/', server.loopback.status());
 
-      var db = client.db(process.env.MONGODB_DB);
-
-      db.collection('StickyListing').find({expired_at: {$gte: new Date() /* not expired uyet */}}).toArray(function (err, result) {
-        if (err) throw err;
-
-        console.log(result);
-        res.send(result);
-      })
+    router.get('/ping', function(req, res) {
+      res.send('pong');
     });
+
+
+    router.get('/GetStickyListings', async function(req, res) {
+      let result = await db.collection('StickyListing').find({expired_at: {$gte: new Date() /* not expired uyet */}}).toArray();
+      res.send(result);
+    });
+    router.get('/GetTopScores', async function(req, res) {
+      res.send('not implemented');
+    });
+
+
+    server.use(router);
+  }).then(() => {
+    console.log(`done `);
+  }).catch((e)=> {
+    console.error(e);
   });
-
-
-  server.use(router);
 
 };
